ebaeb757fe59449e2fb33e476951d93fbbfcbcdb
Vedansh Dwivedi <vedanshdwivedi0@gmail.com>
2022-12-29 03:04:17 +0530

    Initialised Project Router

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,14 @@
 import "./Home.css";
 import React from "react";
+import Navbar from "../../components/navbar/Navbar";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Navbar />
+      Home
+    </div>
+  );
 };
 
 export default Home;
