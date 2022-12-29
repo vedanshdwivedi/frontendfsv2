313c209f12d0d0ffc65983cee485302813a0fe02
Vedansh Dwivedi <vedanshdwivedi0@gmail.com>
2022-12-30 02:59:45 +0530

    Created Login Page UI

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import * as ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
+import Signup from "./pages/signup/Signup";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Register />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,63 @@
-import React from "react";
 import "./Login.css";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+
+  const handleNavigate = (urlPath) => {
+    navigate(urlPath);
+  };
+
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <h1 className="loginTitle">Please enter your login and password!</h1>
+        <div className="loginForm">
+          <div className="loginFormItem">
+            <input type="text" placeholder="Email" />
+          </div>
+          <div className="loginFormItem">
+            <input type="password" placeholder="Password" />
+          </div>
+          <div className="forgotPasswordContainer">
+            <h3 className="forgotPassword">Forgot Password?</h3>
+          </div>
+          <div className="loginButtonWrapper">
+            <button
+              className="loginButton"
+              onClick={() => {
+                handleNavigate("/");
+              }}
+            >
+              Login
+            </button>
+          </div>
+        </div>
+        <div className="socialButtons">
+          <div className="socialButtonItem">
+            <i className="fa-brands fa-facebook"></i>
+          </div>
+          <div className="socialButtonItem">
+            <i className="fa-brands fa-twitter"></i>
+          </div>
+          <div className="socialButtonItem">
+            <i className="fa-brands fa-google"></i>
+          </div>
+        </div>
+        <div className="signUp">
+          <div
+            className="signUpButton"
+            onClick={() => {
+              handleNavigate("/signup");
+            }}
+          >
+            Sign Up
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
