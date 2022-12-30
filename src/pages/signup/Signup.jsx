16af1c6f58aaa00577bef470a12445b16f98a152
Vedansh Dwivedi <vedanshdwivedi0@gmail.com>
2022-12-31 03:16:27 +0530

    Added Signup Page

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,10 +1,95 @@
 import "./Signup.css";
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [lname, setLname] = useState("");
+  const [fname, setFname] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Signup
+  const navigate = useNavigate();
+  const handleNavigate = (urlPath) => {
+    navigate(urlPath);
+  };
+
+  return (
+    <div className="SignupContainer">
+      <div className="signupWrapper">
+        <h1 className="signupTitle">Enter Details to Sign Up</h1>
+        <div className="signupForm">
+          <div className="signupFormItem">
+            <input
+              type="text"
+              placeholder="Email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </div>
+
+          <div className="signupFormItem">
+            <input
+              type="text"
+              placeholder="First Name"
+              onChange={(e) => {
+                setFname(e.target.value);
+              }}
+            />
+          </div>
+          <div className="signupFormItem">
+            <input
+              type="text"
+              placeholder="Last Name"
+              onChange={(e) => {
+                setLname(e.target.value);
+              }}
+            />
+          </div>
+          <div className="signupFormItem">
+            <input
+              type="password"
+              placeholder="Password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </div>
+          <div className="signupButtonWrapper">
+            <button
+              className={
+                email !== "" && password !== "" && fname !== "" && lname !== ""
+                  ? "signupButton"
+                  : "signupButton disabledButton"
+              }
+              onClick={() => {
+                if (
+                  email !== "" &&
+                  password !== "" &&
+                  fname !== "" &&
+                  lname !== ""
+                ) {
+                  handleNavigate("/");
+                }
+              }}
+            >
+              Sign Up
+            </button>
+          </div>
+        </div>
+        <div className="loginButtonWrapper">
+          <div
+            className="loginButtonNavigator"
+            onClick={() => {
+              handleNavigate("/login");
+            }}
+          >
+            Login, if you already have an account
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
