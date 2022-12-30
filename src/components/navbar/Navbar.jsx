55ac03d63913318e380bbedb6fd259e6954b07bd
Vedansh Dwivedi <vedanshdwivedi0@gmail.com>
2022-12-31 03:34:43 +0530

    Added Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,42 @@
 import "./Navbar.css";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const navigate = useNavigate();
+
+  const handleNavigate = (url_path) => {
+    navigate(url_path);
+  };
+
+  const handleLogout = () => {
+    handleNavigate("/login");
+  };
+
+  return (
+    <div className="navbar">
+      <div className="navbarWrapper">
+        <div
+          className="navbarTitle"
+          onClick={() => {
+            handleNavigate("/");
+          }}
+        >
+          FeatureStore
+        </div>
+        <div className="navbarItems">
+          <div className="navbarItem">
+            <div className="logout">
+              <i className="fa-solid fa-power-off"></i>
+              <button className="logoutButton" onClick={handleLogout}>
+                Logout
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
