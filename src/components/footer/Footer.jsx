e308a0cc8d33a43ad64a0e368c2eaac26cfbe08e
Vedansh Dwivedi <vedanshdwivedi0@gmail.com>
2023-01-02 02:08:32 +0530

    Added Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,17 @@
-import './Footer.css'
-import React from 'react'
+import "./Footer.css";
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="Footer">
+      <div className="FooterWrapper">
+        <div className="footerTitle">
+          Made with <i class="fa-solid fa-heart"></i> by Vedansh Dwivedi
+        </div>
+        <div className="footerSubtitle">Copyright &copy; 2023, Feynlabs.ai</div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/pages/createProject/CreateProject.jsx
+++ src/pages/createProject/CreateProject.jsx
@@ -1,8 +1,14 @@
 import "./CreateProject.css";
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/navbar/Navbar";
+import { useNavigate } from "react-router-dom";
+import Footer from "../../components/footer/Footer";
 
 const CreateProject = () => {
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState("");
+
   return (
     <>
       <Navbar />
@@ -20,6 +26,19 @@ const CreateProject = () => {
                 placeholder="A name for your project"
                 required
               />
+            </div>
+            <div className="CreateProjectFormItem">
+              <div className="CreateProjectFormItemLabel">
+                <i className="fa-solid fa-code-branch"></i>
+                <label>Algorithm</label>
+              </div>
+              <select>
+                <option value="xgbregressor" selected>
+                  XG Boost Regressor
+                </option>
+                <option value="svm">Support Vector Machines (SVM)</option>
+                <option value="svm">Decision Trees</option>
+              </select>
             </div>
             <div className="CreateProjectFormItem">
               <div className="CreateProjectFormItemLabel">
@@ -50,6 +69,7 @@ const CreateProject = () => {
           </div>
         </div>
       </div>
+      <Footer />
     </>
   );
 };
